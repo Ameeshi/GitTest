63c5892898020caca0752a9b5eb4d7d0ba9ea7f0
Ameeshi Goel <ameeshi@Ameeshis-MBP.wv.cc.cmu.edu>
2017-04-06 18:52:10 -0400

    Added Header 2

---
+++ git.py
@@ -0,0 +1,14 @@
+# GIT COMMANDS 
+# pwd -where 
+# cd -change dir 
+# mkdir -make dir 
+# touch -new file 
+# ls -list 
+
+
+# GIT 
+# git add . 
+# git commit -m 
+# git log
+# git status 
+# git reset --hard <commit id> 
